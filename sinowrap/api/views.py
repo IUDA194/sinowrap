e6a4346879ece2ed51bc1f15c70272656ded6a2c
Iudà <86917292+IUDA194@users.noreply.github.com>
2023-10-13 23:14:24 +0200

    Fix 12

--- sinowrap/api/views.py
+++ sinowrap/api/views.py
@@ -67,10 +67,11 @@ class help_method:
 
     # Генерим шаблонный жсончик
     def get_data(data, colors : list, i : int = None ):
+        
         result = {
                                 "id": data[i].id,
                                 "name": data[i].name,
-                                "main_photo_path": data[i].main_photo_path,
+                                "main_photo_path": [color.photo_path for color in colors],
                                 "category": data[i].category,
                                 "description": data[i].description,
                                 "manufacturer": data[i].manufacturer,
