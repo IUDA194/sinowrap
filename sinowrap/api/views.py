599ef07a81bdf190c06d8a00bed6da746c2356d3
Iudà <86917292+IUDA194@users.noreply.github.com>
2023-10-21 16:21:11 +0200

    Fix 14

--- sinowrap/api/views.py
+++ sinowrap/api/views.py
@@ -376,7 +376,9 @@ class Position:
             for cat in category:
                 temp_data = []
                 same = position.objects.filter(category=cat)
-                rand_positions = sample(range(0, len(same)), 3)
+                if len(same) == 3: rand_positions = [1,2,2]
+                elif len(same) <= 2: rand_positions = [0,0,0]
+                else: rand_positions = sample(range(0, len(same)), 3)
                 for rand_from_same in rand_positions:
                     colors = help_method.extract_colors(same, rand_from_same)
                     temp_data.append(help_method.get_data(same, colors, rand_from_same))
