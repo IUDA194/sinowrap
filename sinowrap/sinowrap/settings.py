d2fb989ea171fa247eb3df2950f209c69aed98dc
Iudà <86917292+IUDA194@users.noreply.github.com>
2023-10-30 20:40:58 +0100

    Fix 15

--- sinowrap/sinowrap/settings.py
+++ sinowrap/sinowrap/settings.py
@@ -137,9 +137,13 @@ STATIC_URL = 'static/'
 APPEND_SLASH=False
 CORS_ORIGIN_ALLOW_ALL = True
 
+CORS_ALLOW_HEADERS = ('content-disposition', 'accept-encoding',
+                      'content-type', 'accept', 'origin', 'Authorization',
+                      'access-control-allow-methods')
+
 CORS_ORIGIN_WHITELIST = [
     'sinowrap.ru',
- ]
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
