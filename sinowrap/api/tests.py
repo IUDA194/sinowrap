6ae4305b1179a13305b17a5c9b4cbda9b4659906
Iudà <86917292+IUDA194@users.noreply.github.com>
2024-01-05 03:23:11 +0100

    asdasda

--- sinowrap/api/tests.py
+++ sinowrap/api/tests.py
@@ -1,11 +1,1 @@
-import requests
-
-# Замените URL на адрес вашего сервера обработки изображений
-url = "http://127.0.0.1:8000/api/positions/drop_img/?name=dadasda"
-
-# Замените 'path/to/your/image.jpg' на путь к файлу изображения на вашем компьютере
-files = {'image': ('image.jpg', open('/Users/aroslavgladkij/Documents/GitHub/sinowrap/img.png', 'rb'), 'image/jpeg')}
-
-response = requests.post(url, files=files)
-
-print(response.text)
+print("test1.png;test2.png".replace("test1.png", "1.png"))
--- sinowrap/api/urls.py
+++ sinowrap/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from api.views import Position, Bitrix, DRF_viewSet, DRF_viewSet_smart, collect_static
+from api.views import Position, Bitrix, DRF_viewSet, DRF_viewSet_smart, collect_static, rename_static
 
 urlpatterns = [
     path("position/", DRF_viewSet.as_view()),
@@ -10,5 +10,6 @@ urlpatterns = [
     path("positions/category", Position.get_all_category),
     path("positions/random", Position.get_random_id),
     path("positions/random/price", Position.get_all_category_for_price_list),
-    path("buy/", Bitrix.url)
+    path("buy/", Bitrix.url),
+    path("rename/", rename_static.main_url),
 ]
