7a8d162b2963fc94ff9bf9e53bd8ee745bcd6d7e
Iudà <86917292+IUDA194@users.noreply.github.com>
2024-02-19 02:35:11 +0100

    some fix

--- sinowrap/api/serializers.py
+++ sinowrap/api/serializers.py
@@ -55,5 +55,6 @@ class CategorySerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         data = super().to_representation(instance)
         if data['subcategories'] is None:
+            del data['super_category_name']
             data['subcategories'] = []
         return data
