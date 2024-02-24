17f57605875edb2930ff363763b4aee8687cc24a
Iudà <86917292+IUDA194@users.noreply.github.com>
2024-02-24 13:36:16 +0100

    + work bitrix with cart

--- sinowrap/api/views.py
+++ sinowrap/api/views.py
@@ -157,8 +157,8 @@ class help_method:
 
         def send(self) -> dict:
             order_details = ""
+            print(self.cart)
             for item in self.cart:
-                print(item)
                 order_details = order_details + f"{item['product_name']}: {item['count']} шт."
             url = f"https://b24-ow3s7g.bitrix24.ru/rest/1/w6nffiph0rbxh7i3/crm.lead.add.json?FIELDS[TITLE]={self.title}&FIELDS[NAME]={self.name}&FIELDS[LAST_NAME]={self.last_name}&FIELDS[EMAIL][0][VALUE]={self.email}&FIELDS[EMAIL][0][VALUE_TYPE]=WORK&FIELDS[PHONE][0][VALUE]={self.phone}&FIELDS[PHONE][0][VALUE_TYPE]=WORK&FIELDS[OPPORTUNITY]={self.price}&FIELDS[COMMENTS]={order_details}"
             request_data = requests.get(url)
