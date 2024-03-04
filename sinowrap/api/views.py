db22f7664caf642aa823fe734aa0bea62238c623
Iudà <86917292+IUDA194@users.noreply.github.com>
2024-03-04 09:12:05 +0100

    some fix

--- sinowrap/api/views.py
+++ sinowrap/api/views.py
@@ -585,14 +585,3 @@ class DeleteAllCategoriesAPIView(APIView):
     def delete(self, request, format=None):
         category.objects.all().delete()
         return Response({"message": "All categories have been deleted successfully."}, status=204)
-    
-
-lid = help_method.bitrix_lid(
-    title="test",
-    name="test",
-    last_name="test",
-    email="test@gmail.com",
-    phone="test",
-    cart='[{"color_id":0,"product_id":19491,"product_name":"Лента атласная 单色螺纹 2,5 см х 91 м "Однотон"","color_name":"Белый 029","photo_path":"79b8b12e-99ed-11ee-b0ac-ac7deb7f3205.png","count":1,"opt_price":188.5,"total":"11.00"},{"color_id":0,"product_id":18924,"product_name":"Букет Роз 玫瑰花束 7 бутонов "Крупная" 65 см 20/240 шт","color_name":"Ассорти","photo_path":"70103ac8-ab7a-11ee-b0b1-9c6b0010f581.png","count":1,"opt_price":112.45,"total":"440.00"},{"color_id":0,"product_id":18857,"product_name":"Букет Георгин 芍药花束 7 бутонов "Папоротник и выскочки" 35 см 40/800 шт","color_name":"Ассорти","photo_path":"d79cdcd5-b507-11ee-855d-f4c88abe04c8.png","count":1,"opt_price":42.9,"total":"880.00"},{"color_id":0,"product_id":18870,"product_name":"Букет Калл 马蹄莲花束 6 бутонов "Череда" 45 см 40/1400 шт","color_name":"Ассорти","photo_path":"88850d05-a1f8-11ee-b0ad-ac7deb7f3205.png","count":2,"opt_price":37.05,"total":"2680.00"}]',
-    price=11
-).send()
